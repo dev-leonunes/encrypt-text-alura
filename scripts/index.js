2edd7b0afcb29d3cd4e2deacb9251387609b571c
futuroDevLeo <leonunes07@outlook.com>
2024-02-12 15:05:45 -0300

    feat: text encryption and decryption functions created

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,56 @@ const decryptButton = document.getElementById("button2");
 
 const getTextToProcessing = (text) => {
     text = textToProcessing[0].value;
-    console.log(text);
+    return text;
 }
 
-const abc = "abc";
+const processingText = (text) => {
+    text = getTextToProcessing();
+    
+    const textRules = /^[a-z]+$/
+    
+    if (!textRules.test(text)) {
+        return "Erro: O texto está fora do padrão"
+    } else {
+        return text
+    }
+}
 
-encryptButton.onclick = getTextToProcessing;
-decryptButton.onclick = getTextToProcessing;
+const encrypt = (text) => {
+    text = processingText(text);
+
+    const encryptRules = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+
+    const encryptedText = text.split('')
+    .map(letter => encryptRules[letter] || letter)
+    .join('');
+    console.log("🚀 ~ encrypt ~ encryptedText:", encryptedText)
+    return encryptedText;
+}
+
+const decrypt = (text) => {
+    text = processingText(text);
+
+    const decryptRules = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+
+    const decryptedText = text.split(/(enter|imes|ai|ober|ufat)/)
+    .map(part => decryptRules[part] || part)
+    .join('');
+    console.log("🚀 ~ decrypt ~ decryptedText:", decryptedText)
+    return decryptedText;
+}
+
+encryptButton.onclick = encrypt;
+decryptButton.onclick = decrypt;
